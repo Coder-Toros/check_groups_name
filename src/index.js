9190f997192e0e9d9a89118ec8ea1cf46c1513e8
“alex” <poryvvotryv@gmail.com>
2024-02-27 10:22:54 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const arrGroups = [
+  'FE2021-5',
+  'Fe2020-2',
+  'FD2021',
+  'fD2022-1',
+  'FE2020-3',
+  'FD2020/2',
+  'fe2021-3',
+  'FD2020$5',
+  'FE2008-52',
+  'Fm2008-23',
+  'FM2022-1',
+];
+
+// debugger
+
+function checkGroupName(names) {
+  const regEx = /^([A-Z]|[a-z])20\d{2}(-\d+)?$/g;
+  const res = {};
+  for (let groups of names) {
+    res[`${groups}`] = `${regEx.test(groups) ? 'Succsess' : 'Error'}`;
+  }
+  return res;
+}
+
+console.log(checkGroupName(arrGroups));
